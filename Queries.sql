a7ac3f589dddbe26120f595ef89ea1894b9d660e
Valenzione <snyderino@gmail.com>
2015-11-01 18:50:49 +0300

    Очепятка детектед

--- Queries.sql
+++ Queries.sql
@@ -1,15 +1,17 @@
 ----------------------------------------
-АТТЕНШЕН
-Функция vector_text_for_publication(P.publication_ID) была переименована в authors_for_publicationID
+--АТТЕНШЕН
+--Функция authors_for_publicationID(P.publication_ID) была переименована в authors_for_publicationID
 ---------------------------------------
 
 ---------------------------------------
 Поиск по тсвектору
 ---------------------------------------
-SELECT DISTINCT P.publication_title, P.description, P.datePublished, vector_text_for_publication(P.publication_ID) FROM publication as P, authorship as AP, author as A 
-WHERE P.searchable @@ to_tsquery('atom & vector')
+SELECT DISTINCT P.publication_title, P.description, P.datePublished, authors_for_publicationID(P.publication_ID), V.venue_name FROM publication as P, authorship as AP, author as A, venue as V 
+WHERE P.searchable @@ to_tsquery('atom | spin')
 AND A.author_id=AP.author_id
-AND P.publication_id=AP.publication_ID; 
+AND P.publication_id=AP.publication_ID
+AND V.venue_id = P.venue_id
+; 
 
 ---------------------------------------
 Поиск по автору
@@ -24,11 +26,11 @@ AND A.author_name LIKE '%John%';
 --Поиск релевантных артиклов
 ----------------------------------------
 --По роду деятельнссти
-SELECT DISTINCT P.publication_title, vector_text_for_publication(P.publication_ID), P.description, P.datePublished  FROM publication as P, related_by_subject(6002)
+SELECT DISTINCT P.publication_title, authors_for_publicationID(P.publication_ID), P.description, P.datePublished  FROM publication as P, related_by_subject(6002)
 WHERE related_by_subject.id = P.publication_id;
 
 --По автору
-SELECT DISTINCT P.publication_title, vector_text_for_publication(P.publication_ID), P.description, P.datePublished  FROM publication as P, related_by_author(6050)
+SELECT DISTINCT P.publication_title, authors_for_publicationID(P.publication_ID), P.description, P.datePublished  FROM publication as P, related_by_author(6050)
 WHERE related_by_author.id = P.publication_id;
 
 
