ad64dc52b04f7301cd66d126566c6ebe773e52c6
Valenzione <snyderino@gmail.com>
2015-11-01 23:27:47 +0300

    Sample Queries for Documentation

--- Queries.sql
+++ Queries.sql
@@ -1,34 +1,81 @@
 ----------------------------------------
---АТТЕНШЕН
---Функция authors_for_publicationID(P.publication_ID) была переименована в authors_for_publicationID
+-- Sample queries
 ---------------------------------------
 
 ---------------------------------------
-Поиск по тсвектору
+--SmartSearch query with ranking by matching (document_text@@search_text)
 ---------------------------------------
-SELECT *, authors_for_publicationID(P.publication_ID) FROM publication as P
-WHERE P.searchable @@@ to_tsquery('atom & spin')
-; 
-
+SELECT  
+	P.publication_id,
+	ts_headline(P.publication_title,q) 						   as Publication_title,
+	P.datePublished,
+	P.dateUpdated,
+	ts_headline(P.description,q) 							   as Publication_description,
+	ts_headline(authors_for_publicationID(P.publication_ID),q) as Publication_authors,
+	P.venue_id,
+	ts_rank_cd(P.searchable, q) 							   as rank
+FROM 
+	publication 		 AS p,
+	plainto_tsquery('?') AS q
+WHERE
+	P.searchable @@ q
+ORDER BY rank DESC
+;
 
 ---------------------------------------
-Поиск по автору
+--Author Search query
 --------------------------------------
 
-Select * From author as A, publication as P, authorship as AP WHERE 
-AP.publication_id=P.publication_id 
-AND AP.author_id=A.author_id 
-AND A.author_name LIKE '%John%'; 
+Select * From 
+	author as A, 
+	publication as P, 
+	authorship as AP 
+WHERE 
+	AP.publication_id=P.publication_id 
+	AND AP.author_id=A.author_id 
+	AND A.author_name LIKE '%?%'
+LIMIT 1000
+; 
+---------------------------------------
+--Subject Search query
+--------------------------------------
 
+SELECT * FROM 
+	Subject as S, 
+	publication as P, 
+	written_on as SP 
+WHERE 
+	SP.publication_id=P.publication_id 
+	AND SP.subject_id=S.subject_id 
+	AND S.subject_name LIKE '%?%'
+LIMIT 1000
+; 
 ----------------------------------------
---Поиск релевантных артиклов
+--Related publications query
 ----------------------------------------
---По роду деятельнссти
-SELECT DISTINCT P.publication_title, authors_for_publicationID(P.publication_ID), P.description, P.datePublished  FROM publication as P, related_by_subject(6002)
-WHERE related_by_subject.id = P.publication_id;
+--By time and subject
+SELECT DISTINCT 
+	P.publication_title, 
+	authors_for_publicationID(P.publication_ID), 
+	P.description, 
+	P.datePublished  
+FROM
+	publication as P,
+	related_by_subject(?)
+WHERE 
+	related_by_subject.id = P.publication_id;
 
---По автору
-SELECT DISTINCT P.publication_title, authors_for_publicationID(P.publication_ID), P.description, P.datePublished  FROM publication as P, related_by_author(6050)
-WHERE related_by_author.id = P.publication_id;
+--By authors
+SELECT DISTINCT 
+	P.publication_title, 
+	authors_for_publicationID(P.publication_ID), 
+	P.description, 
+	P.datePublished  
+FROM 
+	publication as P, 
+	related_by_author(?)
+WHERE 
+related_by_author.id = P.publication_id
+;
 
 
