01e33a95cf691a9fe94685a671d94e86dc99b7e1
Ilgiz Mustafin <ilgimustafin@gmail.com>
2015-11-22 18:57:38 +0300

    Fix AddPublication servlet

--- src/ru/pumas/AddPublicationServlet.java
+++ src/ru/pumas/AddPublicationServlet.java
@@ -1,48 +1,70 @@
 package ru.pumas;
 
-import sun.util.calendar.BaseCalendar;
-
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.SQLException;
-import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/app/AddPublicationServlet")
 public class AddPublicationServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public AddPublicationServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	public AddPublicationServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String title = request.getParameter("title");
-        String description = request.getParameter("description");
-        String authors = request.getParameter("authors");
-        String subjects = request.getParameter("subjects");
-        String DOI = request.getParameter("DOI");
-        String venue = request.getParameter("venue");
-        String datecreated = request.getParameter("datecreated");
-        String dateupdated = request.getParameter("dateupdated");
-        Integer id=1;
-        /*if (title.equals(null)||description.equals(null)||authors.equals(null)||subjects.equals(null)){
-            response.sendRedirect("error.jsp?from=Please fill all required forms");
-        }*/
-        try {
-            id = DbHelper.addPublication(DOI, null, java.sql.Date.valueOf(datecreated), java.sql.Date.valueOf(dateupdated), null, title, description);
-        } catch (SQLException e) {
-            response.sendRedirect(request.getContextPath() + "error.jsp?from=Please fill all required forms");
-        }
-        response.sendRedirect("/app/publication.jsp?id="+id);
-    }
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String title = request.getParameter("title");
+		String description = request.getParameter("description");
+
+		List<String> authors = new ArrayList<String>();
+		authors.add(request.getParameter("authors"));
+
+		List<String> subjects = new ArrayList<String>();
+		subjects.add(request.getParameter("subjects"));
+
+		String doi = request.getParameter("DOI");
+		String venue = request.getParameter("venue");
+		String dateCreated = request.getParameter("datecreated");
+		String dateUpdated = request.getParameter("dateupdated");
+		String link = request.getParameter("link");
+		Integer id = 1;
+		/*
+		 * if
+		 * (title.equals(null)||description.equals(null)||authors.equals(null)||
+		 * subjects.equals(null)){ response.sendRedirect(
+		 * "error.jsp?from=Please fill all required forms"); }
+		 */
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
+		try {
+			id = DbHelper.makePublication(doi, link,
+					(Date) format.parse(dateCreated),
+					(Date) format.parse(dateUpdated), venue, title, description,
+					authors, subjects);
+		} catch (SQLException e) {
+			response.sendRedirect(request.getContextPath()
+					+ "error.jsp?from=" + e.getMessage());
+		} catch (ParseException e) {
+			response.sendRedirect(request.getContextPath()
+					+ "error.jsp?from=wrong date format");
+			e.printStackTrace();
+		}
+		response.sendRedirect(
+				request.getContextPath() + "/app/publication.jsp?id=" + id);
+	}
 
 }
